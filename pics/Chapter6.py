3dcc8897026db6a44d1bfaf46a861e5d67316c41
nimbus9030 <nimbus9030@gmail.com>
2017-10-31 15:58:12 +0900

    add 2types

---
+++ pics/Chapter6.py
@@ -0,0 +1,16 @@
+def PrintHello():
+    print "hello, python"
+
+def AddNumbers(x, y):
+    print x + y
+
+
+PrintHello()
+# AddNumbers(10, 20)
+# AddNumbers(10, 30)
+# AddNumbers(10, 40)
+# AddNumbers(10, 50)
+# AddNumbers(10, 60)
+#
+# for i in range(10):
+#     AddNumbers(i, 100)
